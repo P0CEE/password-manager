4e8d1e5c34833701344645230c3aaf32a77f49e4
P0CEE <antoine.fromentina@gmail.com>
2025-03-18 10:44:29 +0100

    feature: add tailwind v4

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
 bp = Blueprint('main', __name__, url_prefix='')
 
 @bp.route('/', methods=['GET'])
 def index():
+    return render_template('index.html')
+
+@bp.route('/api/status', methods=['GET'])
+def api_status():
     return jsonify({"status": "online", "message": "Password Manager API is running"})
